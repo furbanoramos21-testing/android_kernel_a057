0288a08e73d15c64934e4287bd68dfa7eadb42c0
Ravindu Deshan <Droidcasts@protonmail.com>
2025-03-07 04:52:59 +0000

    build script: install more requirements
    Signed-off-by: Ravindu Deshan <Droidcasts@protonmail.com>

--- build.sh
+++ build.sh
@@ -13,9 +13,12 @@ if [ -z "$BUILD_KERNEL_VERSION" ]; then
     export BUILD_KERNEL_VERSION="dev"
 fi
 
-#requirements for boot image editor
+# Install requirements
 if [ ! -f ".requirements" ]; then
-    sudo apt install -y git device-tree-compiler lz4 xz-utils zlib1g-dev openjdk-17-jdk gcc g++ python3 python-is-python3 p7zip-full android-sdk-libsparse-utils erofs-utils && touch .requirements
+    sudo apt update && sudo apt install -y git device-tree-compiler lz4 xz-utils zlib1g-dev openjdk-17-jdk gcc g++ python3 python-is-python3 p7zip-full android-sdk-libsparse-utils erofs-utils \
+        default-jdk git gnupg flex bison gperf build-essential zip curl libc6-dev libncurses-dev libx11-dev libreadline-dev libgl1 libgl1-mesa-dev \
+        python3 make sudo gcc g++ bc grep tofrodos python3-markdown libxml2-utils xsltproc zlib1g-dev python-is-python3 libc6-dev libtinfo6 \
+        make repo cpio kmod openssl libelf-dev pahole libssl-dev --fix-missing && touch .requirements
 fi
 
 #setting up localversion
